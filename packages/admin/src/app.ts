543ce8fdfa1b67d0ad9b2a4114983bed27113396
alajfit <alajfit@gmail.com>
2021-03-31 13:38:51 +0100

    Base get for products in DB

--- packages/admin/src/app.ts
+++ packages/admin/src/app.ts
@@ -1,15 +1,27 @@
 import * as express from 'express'
+import { Request, Response } from 'express'
 import * as cors from 'cors'
+import { createConnection } from 'typeorm'
+import { Product } from './entity/product'
 
 const app = express()
 const PORT = 8000
 
-app.use(cors({
-    origin: ['http://localhost:3000']
-}))
+createConnection().then(db => {
+    const productRepo = db.getRepository(Product)
 
-app.use(express.json())
+    app.use(cors({
+        origin: ['http://localhost:3000']
+    }))
 
-app.listen(PORT, () => {
-    console.log(`Server listening on: http://localhost:${PORT}`)
+    app.use(express.json())
+
+    app.get('/api/products', async (req: Request, res: Response) => {
+        const products = await productRepo.find()
+        res.json(products)
+    })
+
+    app.listen(PORT, () => {
+        console.log(`Server listening on: http://localhost:${PORT}`)
+    })
 })
