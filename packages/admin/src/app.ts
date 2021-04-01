958c9ef0875bd78a21f943b209c65594e1cde2dc
alajfit <alajfit@gmail.com>
2021-04-01 19:05:30 +0100

    Re-enabled db access

--- packages/admin/src/app.ts
+++ packages/admin/src/app.ts
@@ -17,20 +17,20 @@ app.use(express.json())
 
 async function main() {
     logger.info(`Initiating Admin Server`)
-    // const db = await createMySQLConnection()
-    // const connection = await connectToRabbitMQ()
-    // const channel = await createRabbitMQChannel(connection)
-    // const productRepo = db.getRepository(Product)
-    // const appRoutes = routes(productRepo, channel)
+    const db = await createMySQLConnection()
+    const connection = await connectToRabbitMQ()
+    const channel = await createRabbitMQChannel(connection)
+    const productRepo = db.getRepository(Product)
+    const appRoutes = routes(productRepo, channel)
 
-    // app.use('/', appRoutes)
+    app.use('/', appRoutes)
 
     app.listen(PORT, () => {
         logger.info(`Server listening on: http://localhost:${PORT}`)
     })
 
-    // process.on('beforeExit', () => {
-    //     connection.close()
-    // })
+    process.on('beforeExit', () => {
+        connection.close()
+    })
 }
 main()
