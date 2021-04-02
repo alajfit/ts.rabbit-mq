13773beb9daa25fcf5d822bca7b3a36353d60f04
alajfit <alajfit@gmail.com>
2021-04-02 20:59:06 +0100

    More updates to admin

--- packages/api/src/app.ts
+++ packages/api/src/app.ts
@@ -2,7 +2,8 @@ import * as express from 'express'
 import * as cors from 'cors'
 import * as pino from 'pino'
 import * as expressPino from 'express-pino-logger'
-import { createMongoDBConnection } from './utils'
+import { Product } from './entity/product'
+import { createMongoDBConnection, connectToRabbitMQ, createRabbitMQChannel } from './utils'
 
 const app = express()
 const PORT = 8001
@@ -16,6 +17,18 @@ app.use(express.json())
 
 async function main() {
     const db = await createMongoDBConnection()
+    const connection = await connectToRabbitMQ()
+    const channel = await createRabbitMQChannel(connection)
+    const productRepo = db.getRepository(Product)
+
+    channel.assertQueue('product_created', { durable: true })
+    channel.assertQueue('product_updated', { durable: true })
+    channel.assertQueue('product_deleted', { durable: true })
+
+    channel.consume('product_created', (msg) => {
+        const eventProduct = JSON.parse(msg.content.toString())
+        console.log('MESSAGE: ', eventProduct)
+    })
 
     app.listen(PORT, () => {
         console.log(`Server listening on: http://localhost:${PORT}`)
